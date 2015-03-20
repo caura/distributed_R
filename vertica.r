abba1a6fa44054d624aa508d5a2b14aab4be318d
Segah Meer <segahm@gmail.com>
2015-03-20 14:56:58 -0700

    all changes before subseeting

--- vertica.r
+++ vertica.r
@@ -8,24 +8,38 @@ connect <- odbcConnect("Vertica")
 data <- sqlQuery(connect, "SELECT COUNT(*) FROM loan_stats AS loans LIMIT 10")
 
 # create a frame with two partitions
-# dc <- dframe(c(333163,100),c(200000,100))
-dc<-dframe(npartitions=c(2,1))
+node_size = 200000
+max_rows = 333163
+options("scipen"=100, "digits"=4)
+dc <- dframe(c(333163,105),c(node_size,105))
+# dc<-dframe(npartitions=c(2,1))
 #iterate dynamically through however many partitions we created (i.e. 2)
 foreach(i, 1:npartitions(dc), initArrays<-function(y=splits(dc,i), index=i) {
   library(vRODBC)
+  options("scipen"=100, "digits"=4)
+  node_size = 200000
+  max_rows = 333163
   connect <- odbcConnect("Vertica")
-  limit <- 10
-  y<-sqlQuery(connect, paste("SELECT * FROM loan_stats AS loans LIMIT",limit,sep=" "))
+  offset <- (node_size * (index - 1))
+  limit <- min(max_rows - offset,node_size)
+  y<-sqlQuery(connect, paste("SELECT * FROM loan_stats AS loans WHERE issue_d < TO_DATE('2013-01-01','YYYY-MM-dd') OFFSET",offset,"LIMIT",limit,sep=" "))
+  y$is_bad <- data.frame(c(1:limit))
+  y$year_issued <- data.frame(c(1:limit))
+  y$month_issued <- data.frame(c(1:limit))
   update(y)
 })
 
 
+  # poor coverage can be used to unset columns for non-parametric algorithms
+  # poor_coverage <- sapply(df, function(x) {
+  #   coverage <- 1 - sum(is.na(x)) / length(x)
+  #   coverage < 0.8
+  # })
+  # df <- df[,poor_coverage==FALSE]
+
 foreach(i, 1:npartitions(dc), initArrays<-function(df=splits(dc,i), index=i) {
-  poor_coverage <- sapply(df, function(x) {
-    coverage <- 1 - sum(is.na(x)) / length(x)
-    coverage < 0.8
-  })
-  df <- df[,poor_coverage==FALSE]
+  library(lubridate)
+  library(stringr)
   bad_indicators <- c("Late (16-30 days)", "Late (31-120 days)", "Default", "Charged Off")
   unique(df$loan_status)
   df$is_bad <- ifelse(df$loan_status %in% bad_indicators, 1,
@@ -39,27 +53,36 @@ foreach(i, 1:npartitions(dc), initArrays<-function(df=splits(dc,i), index=i) {
   df$earliest_cr_line <- as.Date(df$earliest_cr_line)
   df$revol_util <- str_replace_all(df$revol_util, "[%]", "")
   df$revol_util <- as.numeric(df$revol_util)
-  outcomes <- ddply(df, .(year_issued, month_issued), function(x) {
-    c("percent_bad"=sum(x$is_bad) / nrow(x),
-      "n_loans"=nrow(x))
-  })
-  unique(df.term$year_issued)
-  df.term <- subset(df, year_issued < 2013)
-  df.term$home_ownership <- factor(df.term$home_ownership)
-  df.term$is_rent <- df.term$home_ownership=="RENT"
+
+  df$home_ownership <- factor(df$home_ownership)
+  df$is_rent <- df$home_ownership=="RENT"
   # df.term$fico_range <- factor(df.term$fico_range)
   # df.term$fico_ordered <- as.numeric(df.term$fico_range)
   # df.term$fico_range <- factor(df.term$fico_range)
   # df.term$fico_range <- factor(df.term$fico_range_low)
   # df.term$fico_ordered <- as.numeric(df.term$fico_range)
-  idx <- runif(nrow(df.term)) > 0.75
-  train <- df.term[idx==FALSE,]
-  test <- df.term[idx==TRUE,]
   update(df)
 })
 
+# TO DO: FIX
+idx <- runif(nrow(dc)) > 0.75
+train <- dc[idx==FALSE,]
+test <- dc[idx==TRUE,]
+
+  # library(plyr)
+  # outcomes <- ddply(df, .(year_issued, month_issued), function(x) {
+  #   c("percent_bad"=sum(x$is_bad) / nrow(x),
+  #     "n_loans"=nrow(x))
+  # })
+
 library(HPdregression)
-mylogit <- hpdglm(factor(is_bad) ~ last_fico_range_high + last_fico_range_low +
-pub_rec_bankruptcies + revol_util + inq_last_6mths + is_rent, data = train, family = "binomial", na.action=na.omit)
+mylogit <- hpdglm(factor(train$is_bad), 
+                  dframe(train$last_fico_range_high,
+                    train$last_fico_range_low,
+                    train$pub_rec_bankruptcies,
+                    train$revol_util,
+                    train$inq_last_6mths,
+                    train$is_rent), 
+                  family = "binomial", na_action="exclude")
 
 distributedR_shutdown()
