f38fd94446cab676e3b8c2e2c3a94f515d3b469a
Segah Meer <segahm@gmail.com>
2015-04-14 09:31:05 -0700

    cleaned up version

--- vertica.r
+++ vertica.r
@@ -5,39 +5,30 @@ distributedR_start()
 # find the size
 library(vRODBC)
 connect <- odbcConnect("Vertica")
-data <- sqlQuery(connect, "SELECT COUNT(*) FROM loan_stats AS loans LIMIT 10")
+data <- sqlQuery(connect, "SELECT COUNT(*) FROM loan_stats AS loans WHERE issue_d < TO_DATE('2013-01-01','YYYY-MM-dd') LIMIT 1")
 
-# create a frame with two partitions
-node_size = 200000
-max_rows = 333163
+# create a frame with n partitions
+node_size = 10000  #partition size
+max_rows = data$COUNT
 options("scipen"=100, "digits"=4)
-dc <- dframe(c(333163,105),c(node_size,105))
+dc <- dframe(c(max_rows,104),c(node_size,104))
 # dc<-dframe(npartitions=c(2,1))
-#iterate dynamically through however many partitions we created (i.e. 2)
-foreach(i, 1:npartitions(dc), initArrays<-function(y=splits(dc,i), index=i) {
+#iterate dynamically through however many partitions we created
+foreach(i, 1:npartitions(dc), initArrays<-function(y=splits(dc,i), index=i,n_size=node_size,m_rows=max_rows) {
   library(vRODBC)
   options("scipen"=100, "digits"=4)
-  node_size = 200000
-  max_rows = 333163
   connect <- odbcConnect("Vertica")
-  offset <- (node_size * (index - 1))
-  limit <- min(max_rows - offset,node_size)
-  y<-sqlQuery(connect, paste("SELECT * FROM loan_stats AS loans WHERE issue_d < TO_DATE('2013-01-01','YYYY-MM-dd') OFFSET",offset,"LIMIT",limit,sep=" "))
+  offset <- (n_size * (index - 1))
+  limit <- min(m_rows - offset,n_size)
+  y<-sqlQuery (connect, paste("SELECT * FROM loan_stats AS loans WHERE issue_d < TO_DATE('2013-01-01','YYYY-MM-dd') OFFSET",offset,"LIMIT",limit,sep=" "))
   y$is_bad <- data.frame(c(1:limit))
   y$year_issued <- data.frame(c(1:limit))
   y$month_issued <- data.frame(c(1:limit))
+  y$is_rent <- data.frame(c(1:limit))
   update(y)
 })
 
-
-  # poor coverage can be used to unset columns for non-parametric algorithms
-  # poor_coverage <- sapply(df, function(x) {
-  #   coverage <- 1 - sum(is.na(x)) / length(x)
-  #   coverage < 0.8
-  # })
-  # df <- df[,poor_coverage==FALSE]
-
-foreach(i, 1:npartitions(dc), initArrays<-function(df=splits(dc,i), index=i) {
+foreach(i, 1:npartitions(dc), transform<-function(df=splits(dc,i), index=i) {
   library(lubridate)
   library(stringr)
   bad_indicators <- c("Late (16-30 days)", "Late (31-120 days)", "Default", "Charged Off")
@@ -64,16 +55,35 @@ foreach(i, 1:npartitions(dc), initArrays<-function(df=splits(dc,i), index=i) {
   update(df)
 })
 
-# TO DO: FIX
-idx <- runif(nrow(dc)) > 0.75
-train <- dc[idx==FALSE,]
-test <- dc[idx==TRUE,]
+train_partitions <- ceiling(max_rows/node_size*0.75)  #e.g. 8
+test_partitions <- ceiling(max_rows/node_size) - train_partitions #e.g. 2
 
-  # library(plyr)
-  # outcomes <- ddply(df, .(year_issued, month_issued), function(x) {
-  #   c("percent_bad"=sum(x$is_bad) / nrow(x),
-  #     "n_loans"=nrow(x))
-  # })
+train <- dframe(c(train_partitions*node_size,104),c(node_size,104))
+test <- dframe(c(max_rows-train_partitions*node_size,104),c(node_size,104))
+
+#alternative way
+# train <- dframe(npartitions=c(train_partitions,1))    # create new dframe to hold training data set
+# test <- dframe(npartitions=c(test_partitions,1))     # create new dframe to hold test data set
+
+# Following is a cluster-equivalent to this:
+# idx <- runif(nrow(dc)) > 0.75
+# train <- dc[idx==FALSE,]
+# test <- dc[idx==TRUE,]
+
+foreach(i, 1:npartitions(dc), function(
+  dc_i=splits(dc, i), 
+  train_i=splits(train, min(i,train_partitions)), 
+  test_i=splits(test, max(1,i-train_partitions)), 
+  idx=i,
+  split_parts = train_partitions) {
+     if(idx <= split_parts) {                  #Copy 1st max_rows/node_size*0.75 partitions as training data set
+        train_i <- dc_i
+        update(train_i)
+     }else {
+        test_i <- dc_i               # Copy rest max_rows/node_size*0.25 partitions as test data set
+        update(test_i)
+     }
+})
 
 library(HPdregression)
 mylogit <- hpdglm(factor(train$is_bad), 
